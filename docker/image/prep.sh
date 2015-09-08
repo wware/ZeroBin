5e7cd78623431367cdca04b737abd5a52894f047
Will Ware <wware@veracode.com>
2015-09-07 21:45:12 -0400

    Add a few interesting Ubuntu things.

--- docker/image/prep.sh
+++ docker/image/prep.sh
@@ -3,7 +3,8 @@
 # make sure the package repository is up to date
 echo "deb http://archive.ubuntu.com/ubuntu trusty main universe" > /etc/apt/sources.list
 apt-get update
-apt-get install -y git apache2 php5 libapache2-mod-php5 vim
+apt-get install -y openssh-server python-setuptools curl python-redis redis-server python-flask git \
+    apache2 php5 libapache2-mod-php5 vim
 
 easy_install pip virtualenv docopt
 
